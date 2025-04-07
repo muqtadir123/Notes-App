5c4ba88e1d837a8c59fcadf0114cbff11d56930d
Mohd Abdul Muqtadir <48240667+muqtadir123@users.noreply.github.com>
2025-04-07 06:47:02 +0530

    Updated index.js with frontend vercel link

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const app = express()
 // to make input as json
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }))
+app.use(cors({ origin: ["http://localhost:5173","https://notes-app-frontend-wheat.vercel.app"], credentials: true }))
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000")
